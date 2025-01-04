c733bb875a5cd66e50ba64b1eef2a84b91676b37
freya liao <fyjh21317@gmail.com>
2025-01-04 15:32:57 +0800

    feat: implement global styles and transitions with UnoCSS integration

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="flex-c bg-neutral-500 color-red font-size-20 h-100">Noto Sans</div>
+    <div class="flex-c bg-neutral-500 color-red font-size-20 h-100 font-inter">Noto Sans</div>
   </div>
 </template>
