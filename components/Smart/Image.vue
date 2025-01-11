61c0068351b84904b5a415b5b0cdf6d2a8601fa4
freya liao <fyjh21317@gmail.com>
2025-01-11 18:35:02 +0800

    fix: image scaling issue and prevent page overflow by adding width and height properties

--- components/Smart/Image.vue
+++ components/Smart/Image.vue
@@ -1,105 +1,94 @@
-<script lang="ts">
-export default defineComponent({
-  props: {
-    src: {
-      type: String,
-      required: false,
-      default: null
-    },
-    title: {
-      type: String,
-      required: false,
-      default: null
-    },
-    alt: {
-      type: String,
-      required: false,
-      default: null
-    },
-    format: {
-      type: String,
-      required: false,
-      default: 'webp'
-    },
-    fit: {
-      type: String,
-      required: false,
-      default: 'cover'
-    },
-    width: {
-      type: String,
-      required: false,
-      default: null
-    },
-    height: {
-      type: String,
-      required: false,
-      default: null
-    },
-    caption: {
-      type: String,
-      required: false,
-      default: null
-    },
-    optimize: {
-      type: Boolean,
-      required: false,
-      default: true
-    }
+<script lang="ts" setup>
+const props = defineProps({
+  src: {
+    type: String,
+    required: false,
+    default: null
   },
-  setup(props) {
-    const error = ref(false)
-    const loaded = ref(true)
-
-    const getBackgroundUrl = computed(() => {
-      if (error.value || !props.src) return '/icon.png'
-      return props.optimize ? getProxifiedImageUrl(props.src) : props.src
-    })
-
-    const getProxifiedImageUrl = (url: string): string => {
-      if (url.includes('i.imgur.com')) {
-        return `https://proxy.duckduckgo.com/iu/?u=${encodeURIComponent(url)}`
-      }
-      return url
-    }
-
-    const handleError = () => {
-      if (!props.optimize) return
-      error.value = true
-      loaded.value = false
-    }
-
-    return {
-      error,
-      loaded,
-      getBackgroundUrl,
-      getProxifiedImageUrl,
-      handleError
-    }
+  title: {
+    type: String,
+    required: false,
+    default: null
+  },
+  alt: {
+    type: String,
+    required: false,
+    default: null
+  },
+  format: {
+    type: String,
+    required: false,
+    default: 'webp'
+  },
+  fit: {
+    type: String,
+    required: false,
+    default: 'cover'
+  },
+  width: {
+    type: String,
+    required: false,
+    default: null
+  },
+  height: {
+    type: String,
+    required: false,
+    default: null
+  },
+  caption: {
+    type: String,
+    required: false,
+    default: null
+  },
+  optimize: {
+    type: Boolean,
+    required: false,
+    default: true
   }
 })
+
+const error = ref(false)
+const loaded = ref(true)
+
+const getBackgroundUrl = computed(() => {
+  if (error.value || !props.src) return '/icon.png'
+  return props.optimize ? getProxifiedImageUrl(props.src) : props.src
+})
+
+const getProxifiedImageUrl = (url: string): string => {
+  if (url.includes('i.imgur.com')) {
+    return `https://proxy.duckduckgo.com/iu/?u=${encodeURIComponent(url)}`
+  }
+  return url
+}
+
+const handleError = () => {
+  if (!props.optimize) return
+  error.value = true
+  loaded.value = false
+}
 </script>
 
 <template>
   <div
-    v-if="src"
+    v-if="props.src"
     :style="{
       backgroundImage: loaded ? `url(${getBackgroundUrl})` : '',
       backgroundPosition: 'center',
-      backgroundSize: fit
+      backgroundSize: props.fit
     }"
     :class="{
       'bg-gray-100 animate-pulse dark:bg-neutral-700 bg-no-repeat': !loaded,
-      'relative caption': caption
+      'relative caption': props.caption
     }"
-    :title="title || caption"
-    :failed-image-url="error && src"
+    :title="props.title || props.caption"
+    :failed-image-url="error && props.src"
   >
     <img
       :src="getBackgroundUrl"
-      :alt="alt || caption || title || 'image'"
-      :width="width"
-      :height="height"
+      :alt="props.alt || props.caption || props.title || 'image'"
+      :width="props.width"
+      :height="props.height"
       class="invisible"
       loading="lazy"
       @error="handleError"
@@ -107,10 +96,17 @@ export default defineComponent({
     />
 
     <span
-      v-if="caption"
+      v-if="props.caption"
       class="mx-8 text-center text-sm inset-x-0 -bottom-7 text-neutral-400 truncate absolute"
     >
-      {{ caption }}
+      {{ props.caption }}
     </span>
   </div>
 </template>
+
+<style lang="css" scoped>
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
