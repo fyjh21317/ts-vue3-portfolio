e2375c6759846a0999600d27537e3e4d14c6ca8a
freya liao <fyjh21317@gmail.com>
2025-02-19 17:40:10 +0800

    feat: add Smart Figure component

---
+++ components/Smart/Figure.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+const props = defineProps({
+  src: {
+    type: String,
+    default: null
+  },
+  alt: {
+    type: String,
+    default: null
+  },
+  caption: {
+    type: String,
+    default: null
+  },
+  imageClass: {
+    type: String,
+    default: null
+  },
+  border: {
+    type: Boolean,
+    default: false
+  },
+  zoomable: {
+    type: Boolean,
+    default: true
+  }
+})
+</script>
+
+<template>
+  <figure class="w-full h-full overflow-hidden">
+    <img
+      :src="props.src"
+      :alt="props.alt || props.caption || 'image'"
+      loading="lazy"
+      draggable="false"
+      fit="cover"
+      class="object-cover w-full rounded-md"
+      :class="[props.imageClass, props.border && 'border border-black/10 dark:border-white/10']"
+      :data-zoomable="props.zoomable"
+    />
+
+    <figcaption v-if="props.caption" class="text-center">
+      {{ props.caption }}
+    </figcaption>
+  </figure>
+</template>
