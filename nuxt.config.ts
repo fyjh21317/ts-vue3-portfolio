302b7aede07914cb644717d83c6473ea136241a5
freya liao <fyjh21317@gmail.com>
2025-02-27 15:58:24 +0800

    feat: integrate Vuetify and update dependencies in Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@unocss/nuxt', '@nuxtjs/color-mode'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    '@unocss/nuxt',
+    '@nuxtjs/color-mode',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
   colorMode: {
     storageKey: "color-mode",
     preference: "light",
